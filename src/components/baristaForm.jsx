e27ed6291e05573bb0dd26772747c99755f34ca1
rne1223 <rne1223@gmail.com>
2023-09-25 16:25:18 -0700

    final

--- src/components/baristaForm.jsx
+++ src/components/baristaForm.jsx
@@ -1,29 +1,186 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import RecipeChoices from "./recipeChoices";
+import drinksJson from "./drinks.json";
 
 const BaristaForm = () => {
+  const ingredients = {
+    temperature: ["hot", "lukewarm", "cold"],
+    syrup: ["mocha", "vanilla", "toffee", "maple", "caramel", "other", "none"],
+    milk: ["cow", "oat", "goat", "almond", "none"],
+    blended: ["yes", "turbo", "no"],
+  };
 
-    const onNewDrink = (first) => { second };
-    const onCheckAnswer = (first) => { second };
+  const [inputs, setInputs] = useState({
+    temperature: "",
+    milk: "",
+    syrup: "",
+    blended: "",
+  });
 
+  const [currentDrink, setCurrentDrink] = useState("");
+  const [trueRecipe, setTrueRecipe] = useState({});
+  const [correct_temp, setCheckedTemperature] = useState("");
+  const [correct_syrup, setCheckedSyrup] = useState("");
+  const [correct_milk, setCheckedMilk] = useState("");
+  const [correct_blended, setCheckedBlended] = useState("");
+
+  const onNewDrink = () => {
+    setInputs({
+      temperature: "",
+      milk: "",
+      syrup: "",
+      blended: "",
+    });
+
+    getNextDrink();
+
+    setCheckedTemperature("");
+    setCheckedSyrup("");
+    setCheckedMilk("");
+    setCheckedBlended("");
+  };
+
+  // Called when application starts
+  useEffect(() => {
+    onNewDrink();
+  }, [currentDrink]);
+
+  const onCheckAnswer = () => {
+    if (trueRecipe.temp != inputs["temperature"]) {
+      setCheckedTemperature("wrong");
+    } else {
+      setCheckedTemperature("correct");
+    }
+
+    if (trueRecipe.syrup != inputs["syrup"]) {
+      setCheckedSyrup("wrong");
+    } else {
+      setCheckedSyrup("correct");
+    }
+
+    if (trueRecipe.milk != inputs["milk"]) {
+      setCheckedMilk("wrong");
+    } else {
+      setCheckedMilk("correct");
+    }
+
+    if (trueRecipe.blended != inputs["blended"]) {
+      setCheckedBlended("wrong");
+    } else {
+      setCheckedBlended("correct");
+    }
+  };
+
+  const getNextDrink = () => {
+    let randomDrinkIndex = Math.floor(Math.random() * drinksJson.drinks.length);
+
+    setCurrentDrink(drinksJson.drinks[randomDrinkIndex].name);
+    setTrueRecipe(drinksJson.drinks[randomDrinkIndex].ingredients);
+  };
 
   return (
     <div>
-        <h2>Hi, I'd like to order a:</h2>
+      <h2>Hi, I'd like to order a:</h2>
 
-        <form action=""></form>
-        <button type="submit" className="button submit" onClick={onCheckAnswer}>
-            Check Answer
-        </button>
-
-        <button 
-            type="new-drink-button"
-            className="button newdrink"
-            onClick={onNewDrink}
+      <div className="drink-container">
+        <h2 className="mini-header">{currentDrink}</h2>
+        <button
+          type="new-drink-button"
+          className="button newdrink"
+          onClick={onNewDrink}
         >
-            New Drink
+          🔄
         </button>
-    </div>
-  )
-}
+      </div>
 
-export default BaristaForm
+      <form className="container">
+        <div className="mini-container">
+          <h3>Temperature</h3>
+          <div className="answer-space" id={correct_temp}>
+            {inputs["temperature"]}
+          </div>
+          <RecipeChoices
+            handleChange={(e) =>
+              setInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value,
+              }))
+            }
+            label="temperature"
+            choices={ingredients["temperature"]}
+            checked={inputs["temperature"]}
+          />
+        </div>
+
+        <div className="mini-container">
+          <h3>Syrup</h3>
+          <div className="answer-space" id={correct_syrup}>
+            {inputs["syrup"]}
+          </div>
+          <RecipeChoices
+            handleChange={(e) =>
+              setInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value,
+              }))
+            }
+            label="syrup"
+            choices={ingredients["syrup"]}
+            checked={inputs["syrup"]}
+          />
+        </div>
+
+        <div className="mini-container">
+          <h3>Milk</h3>
+          <div className="answer-space" id={correct_milk}>
+            {inputs["milk"]}
+          </div>
+          <RecipeChoices
+            handleChange={(e) =>
+              setInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value,
+              }))
+            }
+            label="milk"
+            choices={ingredients["milk"]}
+            checked={inputs["milk"]}
+          />
+        </div>
+
+        <div className="mini-container">
+          <h3>Blended</h3>
+          <div className="answer-space" id={correct_blended}>
+            {inputs["blended"]}
+          </div>
+          <RecipeChoices
+            handleChange={(e) =>
+              setInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value,
+              }))
+            }
+            label="blended"
+            choices={ingredients["blended"]}
+            checked={inputs["blended"]}
+          />
+        </div>
+
+        <br />
+        <button type="submit" className="button submit" onClick={onCheckAnswer}>
+          Check Answer
+        </button>
+
+        <button
+          type="new-drink-button"
+          className="button newdrink"
+          onClick={onNewDrink}
+        >
+          New Drink
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default BaristaForm;
---
+++ src/components/recipeChoices.jsx
@@ -0,0 +1,25 @@
+
+const RecipeChoices = ({ handleChange, label, choices, checked  }) => {
+
+
+  return (
+    <div className="radio-buttons">
+        {choices && choices.map((choice) => (
+            <li key={choice}>
+                <input 
+                    id={choice}
+                    value={choice}
+                    name={label}
+                    type="radio" 
+                    onChange={handleChange}
+                    checked={checked == choice}
+                />
+                {choice}
+            </li>
+        ))}
+    </div>
+  );
+}
+
+export default RecipeChoices
+
